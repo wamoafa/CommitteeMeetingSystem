3e3d0ca06527c6b56b06f3a21a056c475e7e9b50
Abdulsamad moafa <amoafa@scj.gov.sa>
2024-12-24 11:19:38 +0300

    v5

--- MeetWise/src/Application/Committee/Queries/GetCommitteeById/GetCommitteeByIdQuery.cs
+++ MeetWise/src/Application/Committee/Queries/GetCommitteeById/GetCommitteeByIdQuery.cs
@@ -4,7 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MeetWise.Application.Committee.Queries.GetCommitteeById;
-internal class GetCommitteeByIdQuery
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using MeetWise.Application.Common.Interfaces;
+using MeetWise.Domain.Entities;
+
+public class GetCommitteeByIdQuery : IRequest<CommitteeDto>
 {
+    public int Id { get; set; }
+}
+
+public class GetCommitteeByIdQueryHandler : IRequestHandler<GetCommitteeByIdQuery, CommitteeDto>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetCommitteeByIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CommitteeDto> Handle(GetCommitteeByIdQuery request, CancellationToken cancellationToken)
+    {
+        var committee = await _context.Committees
+            .FindAsync(request.Id);
+
+        if (committee == null || committee.IsDeleted)
+        {
+            throw new NotFoundException(nameof(Committee), request.Id);
+        }
+
+        return new CommitteeDto
+        {
+            Id = committee.Id,
+            Name = committee.Name,
+            Details = committee.Details
+        };
+    }
+}
+
+public class CommitteeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Details { get; set; }
 }
